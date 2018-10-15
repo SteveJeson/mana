c805602aee4748fc85b86257d843da5e58e5339e
German Velasco <germsvel@gmail.com>
2018-10-15 16:16:48 -0400

    Add Blockchain.Account.Inspector
    Adds a `Blockchain.Account.Inspector` module to more easily inspect the
    contents of an account. This is mostly for debugging purposes, and it is
    not expected that it would be used in production. It takes an account
    address (hex) and prints out the balance, nonce, storage root, and code
    hash.

---
+++ apps/blockchain/lib/blockchain/account/inspector.ex
@@ -0,0 +1,35 @@
+defmodule Blockchain.Account.Inspector do
+  @moduledoc """
+  Module to inspect account information
+  """
+
+  alias Blockchain.Account
+
+  def inspect_accounts(state, account_addresses) do
+    Enum.map(account_addresses, &inspect_account(state, &1))
+  end
+
+  def inspect_account(state, account_address) do
+    address_key = Base.decode16!(account_address, case: :mixed)
+    account = Account.get_account(state, address_key)
+
+    if is_nil(account) do
+      IO.puts("#{account_address} does not exist")
+    else
+      """
+      #{account_address}
+        Balance: #{account.balance}
+        Nonce: #{account.nonce}
+        Storage Root:
+          #{encode(account.storage_root)}
+        Code Hash:
+          #{encode(account.code_hash)}
+      """
+      |> IO.puts()
+    end
+  end
+
+  defp encode(binary) do
+    Base.encode16(binary, case: :lower)
+  end
+end
---
+++ apps/blockchain/test/blockchain/account/inspector_test.exs
@@ -0,0 +1,5 @@
+defmodule Blockchain.Account.InspectorTest do
+  use ExUnit.Case, async: true
+
+  doctest Blockchain.Account.Inspector
+end
